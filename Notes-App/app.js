649cae319ac8b3de62d315df747ddf64acbef8eb
Hadar <Hadarr1993@gmail.com>
2022-02-01 18:15:31 +0200

    ver1

---
+++ Notes-App/app.js
@@ -0,0 +1,63 @@
+const validator = require('validator');
+const chalk = require('chalk');
+const yargs = require('yargs')
+const notes = require('./notes');
+
+
+// create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        },
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler:(argv) => {
+        notes.addNote(argv.title , argv.body)
+    }
+})
+
+// create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'removes a note',
+    builder: {
+        title: {
+            describe: 'note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: (argv) => {
+        notes.removeNote(argv.title)
+    }
+})
+
+// create read command
+yargs.command({
+    command: 'read',
+    describe: 'reads a note',
+    handler: () => {
+        console.log('reading a note');
+    }
+})
+// create list notes command
+yargs.command({
+    command:'list',
+    describe:'list all the notes',
+    handler: () => {
+        console.log('listing all existed notes');
+    }
+})
+
+
+yargs.parse()
+
