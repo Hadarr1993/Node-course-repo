fa373a4bfcef660dd05c584f27ec18c33f2e104f
Hadar <Hadarr1993@gmail.com>
2022-03-13 12:46:54 +0200

    bug fix 3

--- web-server/src/utils/geocode.js
+++ web-server/src/utils/geocode.js
@@ -3,15 +3,15 @@ const request = require('request')
 const geocode = (address, callback) => {
     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=pk.eyJ1IjoiaGFkYXJyMTk5MyIsImEiOiJja3pvNnJmc3IzNWExMnVueXBjazl4emd1In0.wbeQzfDsMQTR7OOIGGCYNQ&limit=1`
     
-    request({ url, json: true }, (error, { body }) => {
-        if (error) {
-            callback('Unable to connect to location services!', undefined)
+    request({url, json: true}, (error,{body} = {}) => {
+        if(error) {
+            callback('Unable to connect to location service' , undefined)
         } else if (body.features.length === 0) {
-            callback('Unable to find location. Try another search.', undefined)
+            callback('Unable to find location' , undefined)
         } else {
-            callback(undefined, {
+            callback(undefined , {
                 latitude: body.features[0].center[0],
-                longitude: body.features[0].center[1],
+                longtitude: body.features[0].center[1],
                 location: body.features[0].place_name
             })
         }
