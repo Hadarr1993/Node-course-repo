ee1099e8f69917e57fcf3c36e4d4535d8723a2ec
hadarr1993@gmail.com <Hadarr1993@gmail.com>
2022-04-22 21:59:04 +0300

    new mongo

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,15 +1,32 @@
 require("./db/mongoose");
 const express = require("express");
+const cors = require('cors')
 const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
+const taskRouter = require('./routers/task');
+const missionRouter = require('../../final-Project/src/routers/mission')
+
 
 const app = express();
 const port = process.env.PORT || 3000;
-
+app.use(cors())
 app.use(express.json());
 app.use(userRouter)
 app.use(taskRouter)
+app.use(missionRouter)
 
 app.listen(port, () => {
-  console.log(`Server is live at ${port}`);
+    console.log(`Server is live at ${port}`);
 });
+
+const bcrypt = require('bcryptjs')
+const myFunction = async () => {
+    const password = 'Red12345!'
+    const hashedPassword = await bcrypt.hash(password, 8)
+    console.log(password);
+    console.log(hashedPassword);
+
+    const isMatch = await bcrypt.compare(password , hashedPassword)
+    console.log(isMatch );
+}
+
+myFunction()
